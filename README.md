7c66439f8e10db0f699f150d030efbaa6d4db866
ozerodb <50593663+ozerodb@users.noreply.github.com>
2021-03-25 13:14:33 +0100

    feat: now works for VCs too

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# PoliTO videolezioni downloader
-[Bookmarklet](https://it.wikipedia.org/wiki/Bookmarklet) che permette di avere tutti i link diretti al file `.mp4` delle videolezioni di un corso in una sola pagina.
-I link sono da scaricare a mano, o con un download manager come [JDownloader](http://jdownloader.org/jdownloader2).
+# polito-linkgen
 
-## Come funziona
-### Per installarlo:
-Crea un preferito di qualsiasi pagina -> tasto destro -> modifica.
-Incolla nel campo URL il sorgente di `main.min.js` di questa repo.
+[Bookmarklet](https://it.wikipedia.org/wiki/Bookmarklet) che permette di generare tutti i link diretti ai file `.mp4` delle videolezioni o delle virtual classrooms di un corso.
+Una volta generati i link, si può scaricare ogni video a mano o con un download manager come [JDownloader](https://jdownloader.org/).
 
-### Per usarlo:
-Vai sulla pagina di una qualsiasi videolezione del corso che vuoi scaricare e premi sul preferito. Fatto!
+[Bookmarklet](https://en.wikipedia.org/wiki/Bookmarklet) that allows to generate all the direct links to the `.mp4` files of the videolessons or virtual classrooms of a course.
+Once links are generated, you can download each video manually or by means of a download manager such as [JDownloader](https://jdownloader.org/).
 
-###### Testato solo su Google Chrome!
+### Installazione e utilizzo
+
+- Crea un preferito, poi click destro e modificalo.
+- Incolla nel campo URL il contenuto di `main.js`.
+<!---->
+- Vai nella pagina del corso in cui sono presenti le videolezioni o le virtual classrooms, e clicca sul preferito. Fatto!
+
+### Installation and usage
+
+- Create a bookmark, then right click and modify it.
+- Paste into the URL field the content of `main.js`.
+<!---->
+- Browse to the course's webpage where videolessons or virtual classrooms are located, then click on the bookmark. Done!
+
+##### Questo bookmarklet nasce come estensione di [polito-downloader](https://github.com/lucaceriani/polito-downloader) originariamente sviluppato da [lucaceriani](https://github.com/lucaceriani) (a cui offrirei volentieri un caffè), in principio con l'idea di aggiungere la possibilità di scaricare anche le registrazioni delle virtual classrooms, e in futuro di aggiungere qualche funzionalità un po' più elaborata così da facilitarne l'utilizzo da parte degli utenti meno esperti.
--- main.js
+++ main.js
@@ -1,30 +1,38 @@
-javascript:(function() {
-    var baseUrl="https://didattica.polito.it/portal/pls/portal/";
+javascript: (function () {
     /* creazione attributo replaceAll per le stringhe */
-    String.prototype.replaceAll = function(search, replacement) {
+    String.prototype.replaceAll = function (search, replacement) {
         var target = this;
         return target.replace(new RegExp(search, 'g'), replacement);
     };
 
-    console.log("Getting links...");
-    /* prendo tutto il contetnuo della pagina */
-    var htmlContent=document.documentElement.innerHTML;
-    var reg=/href="(sviluppo\.videolezioni\.vis.*lez=[0-9]*)">/gi;
-    var matches, output = [];
-    /* inserisco i link dentro output */
-    while (matches = reg.exec(htmlContent)) {
-        output.push(matches[1].replaceAll("&amp;", "&"));
-    }
-    /* stampo i link trovati */
-    document.documentElement.innerHTML="";
-    output.forEach(function(item, index){
+    console.log("Retrieving links...");
+    /* prendo tutto il contenuto della pagina */
+    var htmlContent = document.documentElement.innerHTML;
+    /* definisco la regexp da utilizzare */
+    var reg = /href="(sviluppo\.videolezioni\.vis.*lez=[0-9]*)">|href="(sviluppo\.pagina_corso\.main.*VC_[0-9]+_[0-9]+)">/gi;
 
-        console.log((index+1) +" - "+ item);
-        /* se riesco a ottenere il contenuto dell'url eseguo function(data) */
-        $.get(baseUrl+item).success(function(data){
-            var dlLink = data.match(/https:\/\/video\.polito\.it\/dl\/.*\.mp4/gi);
-            /* prendo il primo match */
-            document.write("<a href=\""+dlLink[0]+"\">"+dlLink[0].split("/").pop()+"</a><br>\n");
+    /* cerco i link e li inserisco in output */
+    var matches, output = [];
+    while ((matches = reg.exec(htmlContent)) != null) {
+        /* se il primo capturing group è undefined, allora ha fatto match il secondo */
+        match = (typeof matches[1] != 'undefined') ? matches[1] : matches[2];
+        output.push(match.replaceAll("&amp;", "&"));
+    }
+    if (output.length == 0) {
+        /* mostra alert se non trovo link */
+        alert("Nessuna videolezione/virtual classroom trovata su questa pagina!\n\nNo videolesson/virtual classroom found on this page!");
+    } else {
+        /* stampo i link trovati */
+        document.documentElement.innerHTML = "";
+        output.forEach(function (item, index) {
+
+            console.log("Found link #" + (index + 1) + ": " + item);
+            /* se riesco a ottenere il contenuto dell'url eseguo function(data) */
+            $.get(item).success(function (data) {
+                var dlLink = data.match(/https:\/\/video\.polito\.it\/dl\/.*\.mp4|https:\/\/file\.didattica\.polito\.it\/down\/MATDID_VC\/[^"]+/gi);
+                /* prendo il primo match */
+                document.write("<a href=\"" + dlLink[0] + "\">" + dlLink[0] + "</a><br>\n");
+            });
         });
-    });
+    }
 })()
